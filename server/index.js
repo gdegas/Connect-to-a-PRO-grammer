a011812e2a6fa3375368d0f89d85ebf9b3e5ad4e
gdegas001 <gdegas001@gmail.com>
2017-08-08 14:49:26 -0700

    Added bcrypt to hash the password value.#1

--- server/index.js
+++ server/index.js
@@ -1,23 +1,32 @@
 require('dotenv').config()
-const knex = require('./knex')
+const { postUser } = require('./knex')
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const publicPath = path.join(__dirname, 'public')
 const staticMiddleware = express.static(publicPath)
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 app.use(bodyParser.json())
 app.use(staticMiddleware)
 
 app.post('/users', (req, res) => {
-  const user = req.body
-  knex.postUser(user)
-    .then(user => res.status(201).json(user))
-    .catch(error => {
-      console.log(error)
-      res.sendStatus(500)
-    })
+  const { password } = req.body
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    console.log(hash, err)
+    req.body.password = hash
+    postUser(req.body)
+      .then(user => {
+        res.status(201).json(user)
+      })
+      .catch(error => {
+        console.log(error)
+        res.sendStatus(500)
+      })
+  })
+
 })
 
 app.listen(process.env.PORT, () => console.log('listening on: ' + process.env.PORT))
