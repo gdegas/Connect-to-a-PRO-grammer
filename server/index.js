339993bf126e2e12eb51d7b4d5810c1a5256bd13
gdegas001 <gdegas001@gmail.com>
2017-08-15 20:09:26 -0700

    Finished minor changes.#6

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
-const { postUser, postLanguage } = require('./knex')
-var socket = require('socket.io')
+const { postUser } = require('./knex')
+const socket = require('socket.io')
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -9,6 +9,8 @@ const publicPath = path.join(__dirname, 'public')
 const staticMiddleware = express.static(publicPath)
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.use(bodyParser.json())
 app.use(staticMiddleware)
@@ -16,7 +18,9 @@ app.use(staticMiddleware)
 app.post('/users', (req, res) => {
   const { password } = req.body
   bcrypt.hash(password, saltRounds, function (err, hash) {
-    console.log(err)
+    if (err) {
+      console.log(err)
+    }
     req.body.password = hash
     postUser(req.body)
       .then((user) => {
@@ -29,4 +33,27 @@ app.post('/users', (req, res) => {
   })
 })
 
-app.listen(process.env.PORT, () => console.log('listening on: ' + process.env.PORT))
+const userList = []
+
+io.on('connection', function (socket) {
+  socket.emit('broadcast', userList)
+
+  socket.on('login', function (data) {
+    userList.push(data)
+    console.log('user logged in', userList)
+    const userPosition = userList.length - 1
+    socket.userPosition = userPosition
+    io.sockets.emit('broadcast', userList)
+  })
+
+  socket.on('disconnect', function (data) {
+    console.log('before splice', userList)
+    userList.splice(socket.userPosition, 1)
+    console.log('after splice', userList)
+
+    io.sockets.emit('broadcast', userList)
+  })
+
+})
+
+http.listen(process.env.PORT, () => console.log('listening on: ' + process.env.PORT))
